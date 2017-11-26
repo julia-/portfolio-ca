9eb7f078a75d3a85ec293ddd28407717cb31b520
julia- <julia.ryanx@gmail.com>
2017-11-26 22:03:38 +1100

    add latest cv

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('.menu-toggle').click(function(){
     $('.menu-toggle').toggleClass('active');
     $('.close').toggleClass('active');
-    $('body').toggleClass('noscroll');
+    $('.nav-menu').toggleClass('noscroll');
     $('.nav-menu').toggleClass('active');
     $('.nav-menu').click(function () {
       $('.menu-toggle').toggleClass('active');
@@ -11,11 +11,11 @@ $(document).ready(function(){
       $('.nav-menu').toggleClass('active');
     });
    });
-   
+
    $("#contact-form").submit(function(e){
      e.preventDefault();
      var $form = $(this);
-     
+
      $.post($form.attr("action"), $form.serialize()).then(function(){
        alert("Thank you! I'll be in touch shortly!");
       //  $('.success').toggleClass('active');
