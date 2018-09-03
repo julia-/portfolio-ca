e5370d0d858619d33201c8f5b464e9664da2ab46
julia- <julia.ryanx@gmail.com>
2018-09-03 22:38:04 +1000

    remove navigation menu

--- dest/assets/js/main.js
+++ dest/assets/js/main.js
@@ -1,24 +1,12 @@
 $(document).ready(function() {
-  $('.menu').click(function() {
-    $('.menu').toggleClass('menu--active')
-    $('.menu--close').toggleClass('menu--active')
-    $('.nav').toggleClass('nav--active')
-  })
+  $("#contact-form").submit(function(e) {
+    e.preventDefault();
+    var $form = $(this);
 
-  $('nav').click(function() {
-    $('.menu').toggleClass('menu--active')
-    $('.menu--close').toggleClass('menu--active')
-    $('.nav').toggleClass('nav--active')
-  })
-
-  $('#contact-form').submit(function(e) {
-    e.preventDefault()
-    var $form = $(this)
-
-    $.post($form.attr('action'), $form.serialize()).then(function() {
-      alert("Thank you! I'll be in touch shortly!")
+    $.post($form.attr("action"), $form.serialize()).then(function() {
+      alert("Thank you! I'll be in touch shortly!");
       //  $('.success').toggleClass('active');
-      $('#contact-form')[0].reset()
-    })
-  })
-})
+      $("#contact-form")[0].reset();
+    });
+  });
+});
